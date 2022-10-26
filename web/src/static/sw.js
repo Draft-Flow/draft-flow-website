fef7f2a5d4cf99073c63d6c88c58fcba6a0bbb39
Brett DeWoody <brettdewoody@gmail.com>
2022-10-26 16:32:17 +0100

    Cache fonts in sw

--- web/src/static/sw.js
+++ web/src/static/sw.js
@@ -6,8 +6,17 @@ const {registerRoute} = workbox.routing
 const {CacheFirst} = workbox.strategies
 const {CacheableResponse} = workbox.cacheableResponse
 
+// Cache images
 registerRoute(
   ({request}) => request.destination === 'image',
+  new CacheFirst({
+    plugins: [new CacheableResponse({statuses: [0, 200]})],
+  })
+)
+
+// Cache fonts
+registerRoute(
+  ({request}) => request.destination === 'font',
   new CacheFirst({
     plugins: [new CacheableResponse({statuses: [0, 200]})],
   })
