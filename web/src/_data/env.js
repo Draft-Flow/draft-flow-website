4dd2835650420452202f92b842df9dc7513c0ab5
Brett DeWoody <brettdewoody@gmail.com>
2023-09-05 12:03:13 +0100

    Update env vars

--- web/src/_data/env.js
+++ web/src/_data/env.js
@@ -4,5 +4,5 @@ module.exports = {
   mapbox_token: process.env.MAPBOX_TOKEN,
   mapbox_style: process.env.MAPBOX_STYLE,
   mapbox_style_satellite: process.env.MAPBOX_STYLE_SATELLITE,
-  url: process.env.URL,
+  url: process.env.CONTEXT === 'deploy-preview' ? process.env.DEPLOY_PRIME_URL : process.env.URL,
 }
