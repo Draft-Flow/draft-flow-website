25c06c744b9daed3284c382973a08544533d3a61
Brett DeWoody <brettdewoody@gmail.com>
2025-03-27 21:28:49 +0000

    Add Instagram feed

---
+++ web/src/_data/instagram.js
@@ -0,0 +1,45 @@
+const { imageSize } = require('image-size')
+
+const getInstagram = async () => {
+  const myHeaders = new Headers()
+
+const requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+}
+
+
+const posts = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,permalink,media_url,media_type,timestamp,children{media_url}&limit=20&access_token=${process.env.INSTAGRAM_TOKEN}`, requestOptions)
+  .then(response => response.json())
+  .then(result => result)
+  .catch(error => console.log('error', error))
+
+const postsWithSizes = await Promise.all(
+  await posts.data.map(async (post) => {
+  const image = await fetch(post.media_url)
+    .then(response => response.arrayBuffer())
+    .catch(error => console.log('error', error))
+  
+  const buffer = Buffer.from(image, "utf-8")
+  const dimensions = await imageSize(buffer)
+
+  return {
+    ...post,
+    width: dimensions.width,
+    height: dimensions.height
+  }
+})
+)
+
+  // Refresh the token
+fetch(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${process.env.INSTAGRAM_TOKEN}`)
+.then(response => response.json())
+.then(() => 'Instagram token refreshed')
+.catch(error => console.log('error', error))
+
+  return postsWithSizes
+}
+
+
+module.exports = getInstagram
